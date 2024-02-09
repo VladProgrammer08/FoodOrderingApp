4af998866411c6f9d6b5d464e3c80f16ce846e7e
VladProgrammer08 <bumbuv136@gmail.com>
2024-02-08 17:06:21 -0800

    Added a Model Folder with an OrderMenu starter file

---
+++ FoodOrderingApp/Models/OrderMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrderingApp.Models
+{
+    /// <summary>
+    /// Represents a single ordering menu for affordable purchases.
+    /// </summary>
+    internal class OrderMenu
+    {
+        
+    }
+}
