7e8dae834e60a5e86224be7eb46faf8de117f0f6
mcrowe1992 <135188706+mcrowe1992@users.noreply.github.com>
2024-02-12 07:55:55 -0800

    Added a model/class for the add customer menu

---
+++ FoodOrderingApp/Models/AddCustomer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrderingApp.Models
+{
+    /// <summary>
+    /// Represents a menu for the customer to attach their information to their order
+    /// </summary>
+    internal class AddCustomer
+    {
+        /// <summary>
+        /// A unique id for each customer
+        /// </summary>
+        [Key]
+        public int CustomerId { get; set; }
+
+        /// <summary>
+        /// Customers first name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Customers last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Customers phone number
+        /// </summary>
+        public string PhoneNumber { get; set; }
+    }
+}
