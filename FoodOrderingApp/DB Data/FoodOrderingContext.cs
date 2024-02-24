1188f1e75afa8a420d34a0d98258ff5b5f01cf22
VladProgrammer08 <bumbuv136@gmail.com>
2024-02-23 20:26:12 -0800

    Added DB Context class

---
+++ FoodOrderingApp/DB Data/FoodOrderingContext.cs
@@ -0,0 +1,22 @@
+﻿using FoodOrderingApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrderingApp.DB_Data
+{
+    public class FoodOrderingContext : DbContext
+    {
+        public FoodOrderingContext(DbContextOptions<FoodOrderingContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<AddCustomer> AddCustomers { get; set; }
+
+        public DbSet<OrderMenu> orderMenus { get; set; }
+    }
+}
