9de003a6691949a646cf7c573c8fbc0af235d0c0
VladProgrammer08 <bumbuv136@gmail.com>
2024-02-26 19:40:06 -0800

    Added listBox event that gets customer full name

--- FoodOrderingApp/Forms/AddCustomerForm.Designer.cs
+++ FoodOrderingApp/Forms/AddCustomerForm.Designer.cs
@@ -40,80 +40,78 @@
             // 
             // txtFirstName
             // 
-            txtFirstName.Location = new Point(143, 44);
-            txtFirstName.Margin = new Padding(3, 2, 3, 2);
+            txtFirstName.Location = new Point(163, 59);
             txtFirstName.Name = "txtFirstName";
-            txtFirstName.Size = new Size(182, 23);
+            txtFirstName.Size = new Size(207, 27);
             txtFirstName.TabIndex = 0;
             // 
             // txtLastName
             // 
-            txtLastName.Location = new Point(143, 110);
-            txtLastName.Margin = new Padding(3, 2, 3, 2);
+            txtLastName.Location = new Point(163, 147);
             txtLastName.Name = "txtLastName";
-            txtLastName.Size = new Size(182, 23);
+            txtLastName.Size = new Size(207, 27);
             txtLastName.TabIndex = 1;
             // 
             // txtPhoneNumber
             // 
-            txtPhoneNumber.Location = new Point(143, 178);
-            txtPhoneNumber.Margin = new Padding(3, 2, 3, 2);
+            txtPhoneNumber.Location = new Point(163, 237);
             txtPhoneNumber.Name = "txtPhoneNumber";
-            txtPhoneNumber.Size = new Size(182, 23);
+            txtPhoneNumber.Size = new Size(207, 27);
             txtPhoneNumber.TabIndex = 2;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(32, 49);
+            label1.Location = new Point(37, 65);
             label1.Name = "label1";
-            label1.Size = new Size(64, 15);
+            label1.Size = new Size(80, 20);
             label1.TabIndex = 3;
             label1.Text = "First Name";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(32, 115);
+            label2.Location = new Point(37, 153);
             label2.Name = "label2";
-            label2.Size = new Size(63, 15);
+            label2.Size = new Size(79, 20);
             label2.TabIndex = 4;
             label2.Text = "Last Name";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(32, 183);
+            label3.Location = new Point(37, 244);
             label3.Name = "label3";
-            label3.Size = new Size(88, 15);
+            label3.Size = new Size(108, 20);
             label3.TabIndex = 5;
             label3.Text = "Phone Number";
             // 
             // btnAddCustomer
             // 
-            btnAddCustomer.Location = new Point(302, 244);
-            btnAddCustomer.Margin = new Padding(3, 2, 3, 2);
+            btnAddCustomer.Location = new Point(345, 325);
             btnAddCustomer.Name = "btnAddCustomer";
-            btnAddCustomer.Size = new Size(160, 39);
+            btnAddCustomer.Size = new Size(183, 52);
             btnAddCustomer.TabIndex = 6;
             btnAddCustomer.Text = "Add Customer";
             btnAddCustomer.UseVisualStyleBackColor = true;
+            btnAddCustomer.Click += btnAddCustomer_Click;
             // 
             // btnBack2
             // 
-            btnBack2.Location = new Point(302, 287);
+            btnBack2.Location = new Point(345, 383);
+            btnBack2.Margin = new Padding(3, 4, 3, 4);
             btnBack2.Name = "btnBack2";
-            btnBack2.Size = new Size(160, 39);
+            btnBack2.Size = new Size(183, 52);
             btnBack2.TabIndex = 7;
             btnBack2.Text = "Back";
             btnBack2.UseVisualStyleBackColor = true;
             btnBack2.Click += btnBack2_Click;
             // 
-            // Form2
+            // AddCustomerForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(491, 338);
+            ClientSize = new Size(561, 451);
             Controls.Add(btnBack2);
             Controls.Add(btnAddCustomer);
             Controls.Add(label3);
@@ -122,8 +120,7 @@
             Controls.Add(txtPhoneNumber);
             Controls.Add(txtLastName);
             Controls.Add(txtFirstName);
-            Margin = new Padding(3, 2, 3, 2);
-            Name = "Form2";
+            Name = "AddCustomerForm";
             Text = "Add Customer";
             Load += Form2_Load;
             ResumeLayout(false);
