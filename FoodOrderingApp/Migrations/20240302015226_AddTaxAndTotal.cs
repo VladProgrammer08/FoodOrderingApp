da40c93006df5e7889381df1803a9e82564389d6
VladProgrammer08 <bumbuv136@gmail.com>
2024-03-01 18:13:09 -0800

    Added Tax and OrderTotal to the database

---
+++ FoodOrderingApp/Migrations/20240302015226_AddTaxAndTotal.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodOrderingApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaxAndTotal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "OrderTotal",
+                table: "orderMenus",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Tax",
+                table: "orderMenus",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderTotal",
+                table: "orderMenus");
+
+            migrationBuilder.DropColumn(
+                name: "Tax",
+                table: "orderMenus");
+        }
+    }
+}
