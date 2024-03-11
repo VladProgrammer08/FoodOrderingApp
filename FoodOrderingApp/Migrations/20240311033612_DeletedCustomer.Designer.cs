1b59f8a8f39e366a32a006f050230f58a8a1dc80
VladProgrammer08 <bumbuv136@gmail.com>
2024-03-10 20:42:55 -0700

    Deleted Customer Db and updated Database Context

--- FoodOrderingApp/Migrations/20240311033612_DeletedCustomer.Designer.cs
+++ FoodOrderingApp/Migrations/20240311033612_DeletedCustomer.Designer.cs
@@ -3,6 +3,7 @@ using FoodOrderingApp.DB_Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FoodOrderingApp.Migrations
 {
     [DbContext(typeof(FoodOrderingContext))]
-    partial class FoodOrderingContextModelSnapshot : ModelSnapshot
+    [Migration("20240311033612_DeletedCustomer")]
+    partial class DeletedCustomer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
